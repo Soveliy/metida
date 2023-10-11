5244aa2376cfaaf7557f65844333022597e75780
Soveliy <nikita.elagin2012@yandex.ru>
2023-10-11 21:23:07 +0300

    upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+particlesJS.load('particles-js', 'js/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+let callbackButton = document.querySelector(".callback-button-js");
+let modal = document.querySelector(".modal")
+
+callbackButton.addEventListener('click', function(){
+
+    if(!modal.classList.contains("js-active")){
+       setTimeout(() => {
+        modal.classList.add("js-active")
+       }, 100);
+    }
+})
+
+function closeModal(modal){
+    modal.classList.remove("js-active")
+}
+
+document.addEventListener('click' , function(event){
+    console.log(event)
+
+    if (!modal.contains(event.target) && modal.classList.contains("js-active")){
+        closeModal(modal)
+    }
+})
+
+
+let closeButton = modal.querySelector(".modal__close")
+
+document.addEventListener('click' , function(event){
+    closeModal(modal)
+})
